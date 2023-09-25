99ad9ad9cc4e555b8b3e8e37956a74c7e2c2d3f0
Taha <Taha@gmail.com>
2023-09-25 09:37:14 +0300

    Basics Of Delegates

---
+++ App/App/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+public delegate void MyHandler();
+
+public class Program
+{
+    public static void Test()
+    {
+        Console.WriteLine("I am Test");
+    }
+
+    public  void Foo()
+    {
+        Console.WriteLine("Foo()");
+    }
+    public static void Main()
+    {
+        MyHandler h = new MyHandler(Test);
+        Program P = new Program();
+        MyHandler h2 = new MyHandler(P.Foo);
+        
+        h.Invoke();
+        h2.Invoke();
+
+        Console.WriteLine(h.Method + " " + h.Target);
+        Console.WriteLine(h2.Method + " " + h2.Target);
+    }
+}
