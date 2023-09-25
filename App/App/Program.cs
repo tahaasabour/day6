b31d324a26078ee85b4b1dee69ee8294e6ef9ed8
Taha <Taha@gmail.com>
2023-09-25 10:29:46 +0300

    Lambda Expression, Anaonymous Function

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,29 +1,39 @@
 ﻿
 using System;
-
-public delegate void MyHandler();
+using System.Collections.Generic;
+public delegate bool NumbersFilter(int number);
 
 public class Program
 {
-    public static void Test()
+  
+    public static IEnumerable<int> FilterNumbersByCondition(IEnumerable<int> numbers
+                                , NumbersFilter filter)
     {
-        Console.WriteLine("I am Test");
+        foreach (int number in numbers)
+              if (filter(number))
+                yield return number;
     }
 
-    public  void Foo()
-    {
-        Console.WriteLine("Foo()");
-    }
+
+    
     public static void Main()
     {
-        MyHandler h = new MyHandler(Test);
-        Program P = new Program();
-        MyHandler h2 = new MyHandler(P.Foo);
-        
-        h.Invoke();
-        h2.Invoke();
+        List<int> numbers = new List<int>()
+        {
+            40, 899, 7, 9, 10, 77
+        };
+        // Lambda Expression
+        // Goes To  =>
 
-        Console.WriteLine(h.Method + " " + h.Target);
-        Console.WriteLine(h2.Method + " " + h2.Target);
+        foreach (int num in FilterNumbersByCondition(numbers, i => i > 10))
+        {
+            Console.WriteLine(num);
+        } 
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        foreach (int num in FilterNumbersByCondition(numbers, o => o %2 ==0))
+            Console.WriteLine(num);
     }
 }
